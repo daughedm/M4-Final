bbc10452efd59fe06b4656ee61b41cc4b8eb1a5d
David Daugherty <david.m.daugherty@gmail.com>
2018-07-16 09:31:30 -0600

    Add migrations

--- db/migrations/20180716092618_initial.js
+++ db/migrations/20180716092618_initial.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('marslist', function (table) {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('');
+      table.timestamps(true, true)
+    })
+  ])
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('marslist')
+  ])
 };
