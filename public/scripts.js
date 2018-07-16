77fd67fe61c9b7fe61d709f5218cc9470d68868b
David Daugherty <david.m.daugherty@gmail.com>
2018-07-16 11:13:32 -0600

    Get fetchMarsItems workig and appending to page.

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,32 @@
-const fetchItems
+const fetchMarsList = async () => {
+  try {
+    let response = await fetch('/api/v1/marslist')
+    let marsList = await response.json()
+    let marsListToAppend = marsList.map(listItem => {
+      let isPacked;
+      if (listItem.packed) {
+        isPacked = 'checked';
+      } else {
+        isPacked = null;
+      } 
+
+      console.log(isPacked);
+      return (`
+        <div class="card" id=${listItem.id}>
+          <h3 class="title">${listItem.title}</h3>
+          <input class="Packed-check" type="checkbox" value="Packed" ${isPacked}>
+          <label for="Packed">Packed</label>
+          <button class="delete-button">delete</button>
+        </div>
+      `)
+    })
+    $('.cards-container').append(marsListToAppend)
+
+  } catch (error) {
+    console.log('fetchMarsList error: ', error)
+  }
+}
+
+
+
+fetchMarsList();
