95261d1fccfe414658914fc65aeae0fc7203bb3c
David Daugherty <david.m.daugherty@gmail.com>
2018-07-16 12:23:03 -0600

    Fix updatePackedItem function

--- public/scripts.js
+++ public/scripts.js
@@ -8,17 +8,20 @@ const fetchMarsList = async () => {
     let marsList = await response.json()
     let appendMarsList = marsList.map(listItem => {
       let isPacked;
+      let className;
       if (listItem.packed) {
         isPacked = 'checked';
+        className = 'checked';
       } else {
         isPacked = null;
+        className = 'unchecked';
       } 
 
       console.log(isPacked);
       return (`
         <div class="card" id=${listItem.id}>
           <h3 class="title">${listItem.title}</h3>
-          <input class="Packed-check" type="checkbox" value="Packed" ${isPacked}>
+          <input class="checkbox ${className}" type="checkbox" value="Packed" ${isPacked}>
           <label for="Packed">Packed</label>
           <button class="delete-button">delete</button>
         </div>
@@ -33,16 +36,19 @@ const fetchMarsList = async () => {
 
 const appendMarsItem = (title, packed, id) => {
   let isPacked;
+  let className;
   if (packed) {
     isPacked = 'checked';
+    className = 'checked';
   } else {
     isPacked = null;
+    className = 'unchecked';
   }
 
   $('.cards-container').append(`
       <div class="card" id=${id}>
         <h3 class="title">${title}</h3>
-        <input class="Packed-check" type="checkbox" value="Packed" ${isPacked}>
+        <input class="checkbox ${className}" type="checkbox" value="Packed" ${isPacked}>
         <label for="Packed">Packed</label>
         <button class="delete-button">delete</button>
       </div>
@@ -90,6 +96,53 @@ const deleteMarsItem = async (event) => {
   }
 }
 
+const updatePackedItem = async (event) => {
+  let checkedItem = ($(event.target).parent())
+  let id = $(checkedItem).attr('id')
+  let title = ($(event.target).closest('h3').innerText)
+  let packed;
+  
+  if ($(event.target).hasClass('unchecked')) {
+    packed = true;
+    $(event.target).addClass('checked');
+    $(event.target).removeClass('unchecked');
+
+    try {
+      fetch(`/api/v1/marslist/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({
+          title,
+          packed
+        })
+      })
+    } catch (error) {
+      console.log('updatePackedItem  error: ', error);
+    }
+  } else if (($(event.target).hasClass('checked'))) {
+    packed = false;
+    $(event.target).addClass('unchecked');
+    $(event.target).removeClass('checked');
+
+    try {
+      fetch(`/api/v1/marslist/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          packed
+        })
+      })
+    } catch (error) {
+      console.log('updatePackedItem  error: ', error);
+    }
+  }
+}
+
 $('.add-item-btn').click(addMarsItem)
 $(this).click(deleteMarsItem)
-
+$(this).click(updatePackedItem)
