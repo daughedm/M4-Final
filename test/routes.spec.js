699cb87c2bc713d07319743779340579dbdf06ce
David Daugherty <david.m.daugherty@gmail.com>
2018-07-16 10:15:19 -0600

    Add Get items test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,14 +1,42 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const {app, database} = require('../server');
 
 chai.use(chaiHttp);
 
-describe('Client Routes', () => {
+describe('Endpoint tests', () => {
 
-});
+  beforeEach((done) => {
+    database.migrate.rollback()
+      .then(() => {
+        database.migrate.latest()
+          .then(() => {
+            return database.seed.run()
+              .then(() => {
+                done()
+              })
+          })
+      })
+  })
 
-describe('API Routes', () => {
-
+  it('should GET all the list items', (done) => {
+    chai.request(app)
+      .get('/api/v1/marslist')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('title');
+        response.body[0].title.should.equal('Tacos');
+        response.body[0].should.have.property('packed');
+        response.body[0].packed.should.equal(false)
+        done()
+      })
+  })
 });
